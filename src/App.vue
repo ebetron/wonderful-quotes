aa2328342ab0ec6ded38aea3d19ff6afedcc68b5
ebetron <ebetron@gmail.com>
2019-07-28 16:48:34 -0400

    Header Component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
         <app-new-quote @quoteAdded="newQuote"></app-new-quote>
         <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
         <div class="row">
@@ -13,7 +14,8 @@
 <script>
     import QuoteGrid from './components/QuoteGrid.vue';
     import NewQuote from './components/NewQuote.vue';
-    
+    import Header from './components/Header.vue';
+
     export default {
         data: function(){
             return {
@@ -25,6 +27,9 @@
         },
         methods: {
             newQuote(quote) {
+                if(this.quotes.length >= this.maxQuotes){
+                    return alert('Please delete Quotes first!');
+                }
                 this.quotes.push(quote);
             },
             deleteQuote(index){
@@ -33,7 +38,8 @@
         },
         components: {
             appQuoteGrid: QuoteGrid,
-            appNewQuote: NewQuote
+            appNewQuote: NewQuote,
+            appHeader: Header
         }
     }
 </script>
